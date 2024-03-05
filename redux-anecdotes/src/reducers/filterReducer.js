107dfce5fe7344e8cdd7eec960910da739cfe9fe
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2024-03-06 01:37:32 +0530

    Better anecdotes, step8

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,16 @@
-export const setFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-const reducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+const initialState = ''
+
+const filterSlice = createSlice({
+  name:'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export default reducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
