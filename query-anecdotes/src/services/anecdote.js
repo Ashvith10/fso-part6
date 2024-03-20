fd973953e1c61aea7673d121b247b0e31662e404
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2024-03-20 18:32:13 +0530

    Implement retrieving anecdotes from the server using React Query.

---
+++ query-anecdotes/src/services/anecdote.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+export const getAnecdotes = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
