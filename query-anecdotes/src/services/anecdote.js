576d416e5e98e11601d0fd23927e92c0367162d1
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2024-03-22 01:16:38 +0530

    Implement adding new anecdotes to the server using React Query.

--- query-anecdotes/src/services/anecdote.js
+++ query-anecdotes/src/services/anecdote.js
@@ -8,3 +8,9 @@ export const getAnecdotes = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
+
+export const createAnecdote = async (content) => {
+  const object = { content, id: getId(), votes: 0 }
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
